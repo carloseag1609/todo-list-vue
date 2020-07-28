d4f1626f1bc3d0757df4b688b8844265400ba511
Carlos Aguirre <carloseag1233@gmail.com>
2020-07-28 18:32:36 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+new Vue({
+    el: 'main',
+    data: {
+        newTask: {
+            title: "",
+            description: "",
+            completed: false,
+        },
+        showCompleted: false,
+        tasks: []
+    },
+    methods: {
+        addNewTask() {
+            let { tasks, newTask } = this; 
+            let { title, description, completed } = newTask;
+            if(title === '' || description === '') {
+                alert('You need to specify a title and a description for the new task');  
+                return;
+            } else {
+                tasks.push({
+                    title,
+                    description,
+                    completed
+                });
+            }
+        },
+        deleteTask(task) {
+            if(task.completed) return;
+            if(confirm("Complete task?")) {
+                task.completed = true;
+            }
+        }
+    },
+    computed: {
+        getTasks() {
+            return this.tasks.filter(task => task.completed === this.showCompleted);
+        },
+        getTasksCompleted() {
+            return this.tasks.filter(task => task.completed);
+        }
+    }
+})
